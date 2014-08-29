18834a6de3ae5debe7b981477116a775beac9de8
Neo Chen <neoxen@qq.com>
2014-08-29 11:02:08 +0800

    adding some learning stuff

---
+++ array.js
@@ -0,0 +1,10 @@
+var array = [3,5,12,8,7];
+
+console.log(array);
+
+index = 1;
+array.splice(index, 1);
+console.log(array);
+
+array.splice(2);
+console.log(array);
---
+++ clicolor.js
@@ -0,0 +1,4 @@
+var clc = require('cli-color');
+
+console.log(clc.blue("hello node.js in blue"));
+console.log(clc.red("hello node.js in red"));
